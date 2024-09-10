28d015d6e765bc37ac564e8e0cd0724cd2e9aa5f
LanCaroline <lana.de.sa@hotmail.com>
2024-09-10 19:22:04 -0300

    Start project

---
+++ santanderbank/src/main/java/com/santander/bank/santanderbank/SantanderbankApplication.java
@@ -0,0 +1,13 @@
+package com.santander.bank.santanderbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderbankApplication.class, args);
+	}
+
+}
